eb3b08f1b08244e2ae3f0da88a11a468514de638
MAMAOLWETHU <thulisiweb04@gmail.com>
2023-02-22 14:14:44 +0200

    Add project files.

---
+++ CapenexisLearners23/Migrations/20230222105928_SecondCreate.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CapenexisLearners23.Migrations
+{
+    /// <inheritdoc />
+    public partial class SecondCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "LearnersIdentityNumber",
+                table: "Learners",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "LearnersIdentityNumber",
+                table: "Learners",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+        }
+    }
+}
