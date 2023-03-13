f6f741af9ce50685ac6d63289f5c81b5f9a787f6
MAMAOLWETHU <thulisiweb04@gmail.com>
2023-03-13 12:23:32 +0200

    Adding pages to the nav menu

--- CapenexisLearners23/Controllers/HomeController.cs
+++ CapenexisLearners23/Controllers/HomeController.cs
@@ -22,6 +22,18 @@ namespace CapenexisLearners23.Controllers
         {
             return View();
         }
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
+        public IActionResult Services()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
